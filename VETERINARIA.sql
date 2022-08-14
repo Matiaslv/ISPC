b5e2468ff8201af3cfb7dee9986521507d676c70
jimepar <jime_cor@hotmail.com>
2022-08-14 02:06:23 -0300

    se realizo actividad 1,2y9

---
+++ VETERINARIA.sql
@@ -0,0 +1,130 @@
+CREATE DATABASE  IF NOT EXISTS `veterinaria` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `veterinaria`;
+-- MySQL dump 10.13  Distrib 8.0.19, for Win64 (x86_64)
+--
+-- Host: localhost    Database: veterinaria
+-- ------------------------------------------------------
+-- Server version	8.0.19
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Position to start replication or point-in-time recovery from
+--
+
+-- CHANGE MASTER TO MASTER_LOG_FILE='DESKTOP-OUR94PV-bin.000005', MASTER_LOG_POS=155;
+
+--
+-- Table structure for table `dueno`
+--
+
+DROP TABLE IF EXISTS `dueno`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `dueno` (
+  `DNI` int NOT NULL,
+  `Nombre` varchar(100) DEFAULT NULL,
+  `Apellido` varchar(30) DEFAULT NULL,
+  `Direccion` varchar(100) DEFAULT NULL,
+  `Telefono` varchar(30) DEFAULT NULL,
+  PRIMARY KEY (`DNI`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `dueno`
+--
+-- ORDER BY:  `DNI`
+
+LOCK TABLES `dueno` WRITE;
+/*!40000 ALTER TABLE `dueno` DISABLE KEYS */;
+INSERT INTO `dueno` VALUES (31111111,'walter','Ponce','Libertad123','3512222222');
+/*!40000 ALTER TABLE `dueno` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `historial`
+--
+
+DROP TABLE IF EXISTS `historial`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `historial` (
+  `ID_Historial` int NOT NULL,
+  `Fecha` date DEFAULT NULL,
+  `Descripcion` varchar(100) DEFAULT NULL,
+  `Monto` float(30,30) DEFAULT NULL,
+  `Perro` int DEFAULT NULL,
+  PRIMARY KEY (`ID_Historial`),
+  KEY `fk_PE` (`Perro`),
+  CONSTRAINT `fk_PE` FOREIGN KEY (`Perro`) REFERENCES `perro` (`ID_Perro`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `historial`
+--
+-- ORDER BY:  `ID_Historial`
+
+LOCK TABLES `historial` WRITE;
+/*!40000 ALTER TABLE `historial` DISABLE KEYS */;
+/*!40000 ALTER TABLE `historial` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `perro`
+--
+
+DROP TABLE IF EXISTS `perro`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `perro` (
+  `ID_Perro` int NOT NULL,
+  `Nombre` varchar(100) DEFAULT NULL,
+  `Fecha_nac` date DEFAULT NULL,
+  `Sexo` char(15) DEFAULT NULL,
+  `DNI_DUENO` int DEFAULT NULL,
+  PRIMARY KEY (`ID_Perro`),
+  KEY `fk_DU` (`DNI_DUENO`),
+  CONSTRAINT `fk_DU` FOREIGN KEY (`DNI_DUENO`) REFERENCES `dueno` (`DNI`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `perro`
+--
+-- ORDER BY:  `ID_Perro`
+
+LOCK TABLES `perro` WRITE;
+/*!40000 ALTER TABLE `perro` DISABLE KEYS */;
+INSERT INTO `perro` VALUES (1,'Luli','2015-01-19','femenino',31111111);
+/*!40000 ALTER TABLE `perro` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping events for database 'veterinaria'
+--
+
+--
+-- Dumping routines for database 'veterinaria'
+--
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-08-14  1:40:27
